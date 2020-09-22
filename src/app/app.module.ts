d298087ff5ec25c79ae6cd731d9afc54269d9ee8
Jan-Niklas Voß <jvoss@inovex.de>
2020-09-22 14:42:29 +0200

    chore: update elements to v1.0.2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { InoElementsModule } from '@inovex/elements-angular';
+import { InoElementsModule } from '@inovex.de/elements-angular';
 
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { defineCustomElements } from '@inovex/elements/dist/loader';
+import { defineCustomElements } from '@inovex.de/elements/dist/loader';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';

f4a89deaa9b6f695f7929ffc52ea51e56835912c
Jan-Niklas Voß <jvoss@inovex.de>
2020-10-13 10:19:10 +0200

    Revert "fix: add workaround to make codesandbox work"
    This reverts commit ca96bccc

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,6 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
-// Workaround for Codesandbox issue, see: https://github.com/codesandbox/codesandbox-client/issues/2634
-import "core-js/proposals/reflect-metadata";
-
-
 if (environment.production) {
   enableProdMode();
 }

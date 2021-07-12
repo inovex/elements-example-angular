edc0f8735b19135779fa7b469b0f3ca8d1ee2314
agerlach <agerlach@inovex.de>
2021-07-12 12:37:23 +0200

    feat: add button showcase

---
+++ src/app/showcases/ino-button-showcase/ino-button-showcase.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ino-button-showcase',
+  templateUrl: './ino-button-showcase.component.html',
+  styleUrls: ['./ino-button-showcase.component.scss']
+})
+export class InoButtonShowcaseComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
